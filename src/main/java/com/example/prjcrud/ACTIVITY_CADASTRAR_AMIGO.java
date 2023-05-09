0d581957af9d4010b104e222b0163f49122f8fae
6110574 <6110574@liae.edu.uniube.br>
2023-05-08 22:04:07 -0300

    first commit

---
+++ src/main/java/com/example/prjcrud/ACTIVITY_CADASTRAR_AMIGO.java
@@ -0,0 +1,14 @@
+package com.example.prjcrud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ACTIVITY_CADASTRAR_AMIGO extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastrar_amigo);
+    }
+}
